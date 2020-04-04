c78e2ffe5cffb40fa0d74dc50834c33da41c3645
iulian880 <petcuiulian880@gmail.com>
2020-04-04 20:50:59 +0300

    validating props

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -8,7 +8,13 @@
 
 <script>
     export default {
-        props: ['myName'],
+        props: {
+            myName: {
+                type: String,
+                required: true,
+                //default: 'username'
+            }
+        },
         methods: {
             switchName() {
                 return this.myName.split("").reverse().join("")
